67c578e1492fd9ae3167932ab0a2fe9015188866
ashutosh <ashutoshsinha519@gmail.com>
2023-06-15 15:30:49 +0530

    code fix

--- Recurssion 1/isSorted.py
+++ Recurssion 1/isSorted.py
@@ -21,6 +21,20 @@ def isSorted2(a):
             return True
     else:
         return False
+    
+
+
+# without making copy of the array for evey recursion calls
+def isSorted3(a , idx):
+    if idx == len(a) or idx == len(a) - 1:
+        return True
+    if a[idx] > a[idx+1]:
+        return False
+    rans = isSorted3(a , idx+1)
+    return rans
+
+
+
 if __name__ == '__main__':
-    a = [x for x in input('enter the values for array: ').split()]
-    print(isSorted(a))
+    a = [int(x) for x in input('enter the values for array: ').split()]
+    print(isSorted3(a , 0))
