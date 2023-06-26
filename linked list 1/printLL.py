c35466d7d75344c865bf62d2128e5a35c0215fed
ashutosh <ashutoshsinha519@gmail.com>
2023-06-26 23:35:47 +0530

    code update

--- linked list 1/printLL.py
+++ linked list 1/printLL.py
@@ -23,7 +23,7 @@ def printLL(head):
     while head is not None:
         print(head.data , '->' , end = ' ')
         head = head.next
-    print(-1)
+    print('None')
 
 
 if __name__ == '__main__':
